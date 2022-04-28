10fb319cb525fa33bf0cecca2e8d7952f732cf8f
ShivanMavaCore369 <104573016+ShivanMavaCore369@users.noreply.github.com>
2022-04-28 10:29:05 -0300

    Add files via upload

---
+++ PrimitiveDataTypes.java
@@ -0,0 +1,24 @@
+public class PrimitiveDataTypes {
+  public static void public static void main(String[] args) {
+    
+  }
+}
+// Bij PrimitiveDataTypes heb je totaal 6 strings namelijk: 1. byte, 2. short, 3.int, 4.long, 5. float, 6. double
+
+// -128 to 127
+byte mybyte = 12;
+
+// -32, 768, to 32, 676 = Niet elk keer wordt het gebruikt
+short myshort = 1234;
+
+// -2, 147, 483, 648, to 2, 147, 483, 647
+int myInt = 2837;
+
+// -9, 223, 372, 036, 854, 775, 808, to 9, 223, 372, 036, 854, 775, 807
+long mylong = 2837123123l; // Bij long moet je altijd aan het eind een l plaatsen.!!
+
+// 6 decimaal cijfers
+float myfloat = 6.3f; // bij float moet je altijd aan het eind een f plaatsen
+
+// 15 decimaal cijfers
+double myDouble = 45.213434d; // bij dubble ook het zelfde, aan het eind een d plaatsen
